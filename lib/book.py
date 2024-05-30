2f2db11c7d128e3e08862f3942ec7460c769d518
tonymk0987 <k.tony.muriuki@gmail.com>
2024-05-31 02:06:33 +0300

    Complete

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self,title="And Then There Were None",page_count=272):
+        self.title=title
+        self.page_count=page_count
+
+    @property
+    def book(self):
+        return self.title
+    
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!" )
+
+
+    
+
+
+  
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
 class Shoe:
-    pass
+    def __init__(self, brand="", size=""):
+        self._brand = brand
+        self._size = size
+        self._condition = "New"
+
+    @property
+    def brand(self):
+        return self._brand
+    
+    @brand.setter
+    def brand(self, brand):
+        self._brand = brand
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    @property
+    def condition(self):
+        return self._condition
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self._condition = "New"
+
+    
+    
+
